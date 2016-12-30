73f6cadb5d76fc2465d7685b051fab60cd329920
Tae Hoon Lee <thlee1122@hotmail.com>
2016-12-30 16:29:30 -0500

    Added Countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
 	render: function() {
 		return (
-			<p>Countdown.jsx</p>
+			<div>
+				<Clock totalSeconds={129}/>
+			</div>
 		)
 	}
 });
