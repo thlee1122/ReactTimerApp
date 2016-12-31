58a5199d5f55e7b2091b1b3d0cca1c828b0646bb
Tae Hoon Lee <thlee1122@hotmail.com>
2016-12-30 23:18:25 -0500

    Control function

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -1,32 +1,31 @@
-var expect = require('expect');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
 var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
 var Controls = require('Controls');
 
-describe ('Controls', () => {
-	it('should exist', () => {
-		expect(Controls).toExist();
-	});
+describe('Controls', () => {
+  it('should exist', () => {
+    expect(Controls).toExist();
+  });
 
-	describe('render', () => {
-		it('should render pause when started', () => {
-			var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
-			var $el = $(ReactDOM.findDOMNode(controls));
-			var $pauseButton = $el.find('button:contains(Pause)');
+  describe('render', () => {
+    it('should render pause when started', () => {
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Pause)');
 
-			expect($pauseButton.length).toBe(1);
-		});
+      expect($pauseButton.length).toBe(1);
+    });
 
-		it('should render start when paused', () => {
-			var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
-			var $el = $(ReactDOM.findDOMNode(controls));
-			var $pauseButton = $el.find('button:contains(Start)');
+    it('should render start when paused', () => {
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Start)');
 
-			expect($pauseButton.length).toBe(1);
-		});
-
-	});
+      expect($pauseButton.length).toBe(1);
+    });
+  });
 });
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -1,38 +1,38 @@
-var expect = require('expect');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
 var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
 var Countdown = require('Countdown');
 
 describe('Countdown', () => {
-	it('should exist', () => {
-		expect(Countdown).toExist();
-	});
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
 
-	describe('handleSetCountdown', () => {
-		it('should set state to started and count down', (done) => {
-			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-			countdown.handleSetCountdown(10);
+  describe('handleSetCountdown', () => {
+     it('should set state to started and coutdown', (done) => {
+       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+       countdown.handleSetCountdown(10);
 
-			expect(countdown.state.count).toBe(10);
-			expect(countdown.state.countdownStatus).toBe('started');
+       expect(countdown.state.count).toBe(10);
+       expect(countdown.state.countdownStatus).toBe('started');
 
-			setTimeout(() => {
-				expect(countdown.state.count).toBe(9);
-				done();
-			}, 1001)
-		});
+       setTimeout(() => {
+         expect(countdown.state.count).toBe(9);
+         done();
+       }, 1001)
+     });
 
-		it('should not countdown when count is 0', (done) => {
-			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-			countdown.handleSetCountdown(1);
+     it('should never set count less than zero', (done) => {
+       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+       countdown.handleSetCountdown(1);
 
-			setTimeout(() => {
-				expect(countdown.state.count).toBe(0);
-				done();
-			}, 3001)
-		})
-	});
+       setTimeout(() => {
+         expect(countdown.state.count).toBe(0);
+         done();
+       }, 3001)
+     });
+  });
 });
